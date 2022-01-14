0de47b8b20fb82ed21969924abd62f0d24341762
Jordyn <jordyn@example.com>
2022-01-14 11:52:22 -0500

    Created the component folder, GroceryCard and GroceryList compoent and got the fakeData to render to the page

---
+++ grocerease/src/components/GroceryCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const GroceryCard = (props) => {
+    let { category, name, image } = props
+
+    return (
+        <div>
+            <h1>{category}</h1>
+            <p>{name}</p>
+            <p>{image}</p>
+        </div>
+    )
+}
