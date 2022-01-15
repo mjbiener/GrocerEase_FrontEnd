e0059457c1087dad6cac60bd8d64287524220b32
Marsigli <marsigli1988@gmail.com>
2022-01-15 10:01:38 -0500

    added css styling to register component

--- grocerease/src/components/Register.js
+++ grocerease/src/components/Register.js
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import axios from 'axios'
-
+import { useState } from 'react';
+import axios from 'axios';
+import '../register.css';
 
 export const Register = ({ setAuth }) => {
     const [password, setPassword] = useState('')
@@ -44,15 +44,39 @@ export const Register = ({ setAuth }) => {
         }
 
         return (
-            <div>
-                <form onSubmit={handleSubmit}>
-                    <label>Email</label>
-                    <input
-                        className=""
-                        type="text"
-                        placeholder="JohnSmith@gmail.com"
-                        value={email}
-                        onChange={(e) => handleChange('email', e)}/>
+            <div className='register_container'>
+                <form className='register_form' onSubmit={handleSubmit}>
+                    <div className='username_container'>
+                        <label className="username_label">Username:</label>
+                        <input className='username_input' 
+                            type="text"
+                            placeholder="JohnSmith88"
+                            value={username}
+                            onChange={(e) => handleChange('username', e)}/>
+                    </div>
+                    <div className='password_container'>
+                        <label className='password_label'>Password:</label>
+                        <input className="password_input"
+                            placeholder="password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => handleChange('password', e)}
+                        />
+                    </div>
+                    <div className='email_container'>
+                        <label className='email_label'>Email:</label>
+                        <input className='email_input'
+                            type="text"
+                            placeholder="JohnSmith@gmail.com"
+                            value={email}
+                            onChange={(e) => handleChange('email', e)}/>
+                    </div>
+                    <div>
+                        <button type="submit">
+                            Create Account
+                        </button>
+                    </div>
+                    <a href="/">Forgot Username</a>
                 </form>
             </div>
         )}
