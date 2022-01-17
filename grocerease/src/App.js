80c57eb8dbfaa89d045dad79d69fd25d5c683960
Marsigli <marsigli1988@gmail.com>
2022-01-16 19:20:41 -0500

    added react router path for homepage and lists page, added homepage component that returns header, brief desc, login link, and register link

--- grocerease/src/App.js
+++ grocerease/src/App.js
@@ -7,15 +7,31 @@ import './App.css'
 import { useState } from 'react'
 import { GroceryList } from './components/GroceryList'
 import { GroceryCard } from './components/GroceryCard'
+import useLocalStorageState from 'use-local-storage-state'
+import Homepage from "./components/Homepage";
 
 export default function App() {
+  const [token, setToken] = useLocalStorageState('grocereaseAuthToken', null)
+  const [username, setUsername] = useLocalStorageState('grocereaseUsername', '')
+
+  function setAuth(username, token) {
+    setUsername(username)
+    setToken(token)
+  }
+
+  const isLoggedIn = username && token
+
   return (
     <Router>
       <Routes>
-        <Route path="/register" element={<Register />} />
+        <Route path='/' element={ isLoggedIn ? <GroceryList/> : <Homepage />}></Route>
+        <Route path='/lists'></Route>
+        <Route path="/register" element={<Register setAuth={setAuth} />} />
         <Route path="/forgot_username" element={<ForgotUsername />} />
       </Routes>
     </Router>
+    
   );
 }
+    
 
