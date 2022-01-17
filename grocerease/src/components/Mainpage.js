4075cfa43ad311113d595ad494451ec439b154d6
Marsigli <marsigli1988@gmail.com>
2022-01-17 10:59:35 -0500

    test

--- grocerease/src/components/Mainpage.js
+++ grocerease/src/components/Mainpage.js
@@ -2,19 +2,19 @@ import Navbar from "./Navbar";
 import GroceryList from "./GroceryList";
 
 const Mainpage = () => {
-  return (
-    <>
-    <div>
-        <h1>GrocerEase</h1>
-    </div>
-    <div className="navbar_container">
-        <Navbar />
-    </div>
-    <div>
-        <GroceryList />
-    </div>
-    </>
-  );
+    return (
+        <>
+        <div>
+            <h1>GrocerEase</h1>
+        </div>
+        <div className="navbar_container">
+            <Navbar />
+        </div>
+        <div>
+            <GroceryList />
+        </div>
+        </>
+);
 };
 
 export default Mainpage;
