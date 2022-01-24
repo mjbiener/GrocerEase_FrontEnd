367db86a69553338b28fc4059583d982abfc84dd
Marsigli <marsigli1988@gmail.com>
2022-01-24 17:51:28 -0500

    removed forgotUsername and forgotPassword pages

--- grocerease/src/App.js
+++ grocerease/src/App.js
@@ -1,13 +1,11 @@
 import { CreateListForm } from './components/CreateListForm';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Register } from "./components/Register";
-import ForgotUsername from "./components/ForgotUsername";
 import useLocalStorageState from 'use-local-storage-state';
 import Homepage from "./components/Homepage";
 import Mainpage from "./components/Mainpage";
 import Createlist from "./components/CreateList";
 import Login from './components/Login.js';
-import ForgotPassword from './components/ForgotPassword';
 import Logout from './components/Logout';
 import GroceryListDetail from './components/GroceryListDetail';
 import SavedGroceryList from './components/SavedGroceryList';
@@ -35,10 +33,8 @@ export default function App() {
         <Route path='/' element={ isLoggedIn ? <Mainpage username={username} token={token} eraseAuth={eraseAuth}/> : <Homepage />}></Route>
         <Route path='/lists' element= {<Mainpage username={username} token={token} eraseAuth={eraseAuth}/>} />
         <Route path="/register" element= { isLoggedIn ? <Mainpage username={username} token={token} eraseAuth={eraseAuth}/> : <Register setAuth={setAuth} username={username} token={token} isLoggedIn={isLoggedIn}/>} />
-        <Route path="/forgot_username" element={<ForgotUsername />} />
         <Route path='/logout' element={<Logout username={username} eraseAuth={eraseAuth} token={token}/>}/>
         <Route path="/login" element={ <Login setAuth={setAuth} isLoggedIn={isLoggedIn}/>} />
-        <Route path="/forgotPassword" element={<ForgotPassword />}/>
         <Route path="/saved_list" element={<SavedGroceryList token={token} />} />
         <Route path="/go_shopping"/>
         <Route path="/unlisted_item" />
