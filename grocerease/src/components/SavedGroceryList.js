3fcd86e069cf15d278fb13328ffb9d9b7872dd48
Jordyn <jordyn@example.com>
2022-01-26 19:09:32 -0500

    saving

--- grocerease/src/components/SavedGroceryList.js
+++ grocerease/src/components/SavedGroceryList.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 import { GroceryCard } from './GroceryCard'
-import _ from "lodash";
-
+import _ from 'lodash'
 
 const SavedGroceryList = ({ token }) => {
     const [lists, setLists] = useState([])
@@ -13,53 +12,66 @@ const SavedGroceryList = ({ token }) => {
                 headers: {
                     'Content-Type': 'application/json',
                     Authorization: `token ${token}`,
-                }
+                },
             })
             .then((res) => {
-                setLists(res.data)
-                console.log(res.data)
+                let saved_lists_from_server = res.data
+                console.log('LISTS from SERVER')
+                console.log(saved_lists_from_server)
+                const sorted_lists = _.orderBy(
+                    saved_lists_from_server,
+                    ['name'],
+                    ['asc']
+                )
+                console.log({ sorted_lists })
+                setLists(sorted_lists)
             })
-            .catch(error => console.log(error))
+            .catch((error) => console.log(error))
     }, [token, setLists])
 
-
-    _.orderBy(lists, ['name'], ['asc', 'desc']);
-
+    const DeleteList = (listId) => {
+        axios
+            .delete(
+                `https://grocerease.herokuapp.com/grocerease/delete_list/${listId}`,
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `token ${token}`,
+                    },
+                }
+            )
+            .then((res) => {
+                setLists(lists.filter((list) => list.pk !== listId))
+            })
+    }
 
     return (
 
-        //  update the state of items need the sort function (lodash ), order by, sort by, collection: sort by, order by or group by 
-        <div>
-            <div className="search-filter">
+        
+        
+        <div className='groceryList_container'>
+            {lists &&
+                lists.map((list) => {
+                    console.log(list)
+                    return (
+                        <GroceryCard
+                            name={list.name}
+                            date_created={list.date_created}
+                            tags={list.tags}
+                            listId={list.pk}
+                            onDelete={DeleteList} />
+                    )
+                })}
+            {/* <div className="search-filter">
                 <div>
                     <label>Sort By:</label>
-
                     <select className="sort-by">
                         <option value="">Select one</option>
                         <option value="name">Title</option>
-                        <option value="date">Date</option>
                     </select>
-
- 
-
                 </div>
-            </div>
-
-
-            <div className='grocery-list'>
-                {lists &&
-                    lists.map((list) => {
-                        console.log(list)
-                        return (
-                            <GroceryCard
-                                name={list.name}
-                                date_created={list.date_created}
-                                tags={list.tags}
-                                listId={list.pk}
-                            />
-                        )
-                    })}
-
+            </div> */}
+            <div>
             </div>
         </div>
     )
