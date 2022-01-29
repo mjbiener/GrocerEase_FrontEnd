7de3dd0872db9dfae243662b273a21d65df0efe0
Jordyn <jordyn@example.com>
2022-01-29 16:21:04 -0500

    styling of the home page

--- grocerease/src/components/GroceryListDetail.js
+++ grocerease/src/components/GroceryListDetail.js
@@ -5,11 +5,12 @@ import GroceryListItem from './GroceryListItem'
 import { useLocation, useNavigate } from 'react-router-dom'
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
-import Container from '@mui/material/Container';
+import { Button, CardActions, Container } from '@mui/material';
 
 
-const GroceryListDetail = ({token}) => {
-    const location = useLocation() 
+
+const GroceryListDetail = ({ token }) => {
+    const location = useLocation()
     let listId = location.search.split('=')[1]
     const [value, setValue] = useState('');
     const [items, setItems] = useState([]);
@@ -19,111 +20,114 @@ const GroceryListDetail = ({token}) => {
 
     useEffect(() => {
         axios.get(`https://grocerease.herokuapp.com/grocerease/list_detail/${listId}`,
-        {
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `token ${token}`
-            }
-        })
-        .then (res => {
-            setListName(res.data.name)
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `token ${token}`
+                }
+            })
+            .then(res => {
+                setListName(res.data.name)
 
-        })
+            })
         axios.get(`https://grocerease.herokuapp.com/grocerease/lists/${listId}/items/`,
-        {
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `token ${token}`
-            }
-        })
-        .then (res => {
-            console.log(res, 'get items')
-            const newItems = [...items, ...res.data]
-            setItems(newItems)
-        })
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `token ${token}`
+                }
+            })
+            .then(res => {
+                console.log(res, 'get items')
+                const newItems = [...items, ...res.data]
+                setItems(newItems)
+            })
     },
-    []
+        []
     )
     const onAddProduct = (event) => {
         event.preventDefault()
         console.log(choices)
-        axios.post(`https://grocerease.herokuapp.com/grocerease/lists/${listId}/items/`, 
-        {   name: value,
-            quantity: 1,
-            choices: choices,
-        },
-        {
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `token ${token}`
+        axios.post(`https://grocerease.herokuapp.com/grocerease/lists/${listId}/items/`,
+            {
+                name: value,
+                quantity: 1,
+                choices: choices,
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `token ${token}`
+                }
             }
-        }
-        ).then (res => {
+        ).then(res => {
             console.log(res, 'items endpoint')
             setItems([
                 ...items,
-                {name: res.data.name, item_quantity: res.data.item_quantity, choices: res.data.choices}
+                { name: res.data.name, item_quantity: res.data.item_quantity, choices: res.data.choices }
             ])
         })
     }
     const saveList = () => {
         axios.patch(`https://grocerease.herokuapp.com/grocerease/edit_list/${listId}/`,
-        {
-            name: listName,
-        },
-        {
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `token ${token}`
+            {
+                name: listName,
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `token ${token}`
+                }
             }
-        }        
-        ).then( () => {
+        ).then(() => {
             navigate('/lists')
         })
     }
 
     return (
         <>
-                <React.Fragment>
-            <CssBaseline />
+            <React.Fragment>
+                <CssBaseline />
 
-            <Container style={{ backgroundColor: "#FFF8F0"  }}>
-            <div className='list_detail_container'>
-                <input className='pa2 input-reset ba bg-transparent w-100 measure search_input list_name_category_input' onChange={(event) => setListName(event.target.value)} value={listName}/>
+                <Container style={{ backgroundColor: "#FFF8F0" }}>
+                    <div className='list_detail_container'>
+                        <input className='pa2 input-reset ba bg-transparent w-100 measure search_input list_name_category_input' onChange={(event) => setListName(event.target.value)} value={listName} />
 
-                <select className='' onChange={(event) => setChoices(event.target.value)} value={choices}>
-                    <option value='Produce'>Produce</option>
-                    <option value="Dairy">Dairy</option>
-                    <option value="Baked Goods">Baked Goods</option>
-                    <option value="Meat and Fish">Meat and Fish</option>
-                    <option value="Snacks">Snacks</option>
-                    <option value="Alcohol">Alcohol</option>
-                    <option value="Baby Care">Baby Care</option>
-                </select>
-            </div>
-            
-            <div className='add_product_container'>
-                <div>
-                    <input className='pa2 input-reset ba bg-transparent w-100 measure add_product_input' type="text" id="products" value={value}
-                    placeholder='Search for products'
-                    onChange={(event) => setValue(event.target.value)}></input>
-                </div>
+                        <select className='' onChange={(event) => setChoices(event.target.value)} value={choices}>
+                            <option value='Produce'>Produce</option>
+                            <option value="Dairy">Dairy</option>
+                            <option value="Baked Goods">Baked Goods</option>
+                            <option value="Meat and Fish">Meat and Fish</option>
+                            <option value="Snacks">Snacks</option>
+                            <option value="Alcohol">Alcohol</option>
+                            <option value="Baby Care">Baby Care</option>
+                        </select>
+                    </div>
+                </Container>
 
-                <div>
-                    <button className='add_product_button' onClick={onAddProduct} type='submit'>Add Product</button>
-                </div>
-            </div>
-            <div className='button_container'>
-                <button className='save_list_button' onClick={saveList} >Save List</button>
-                <button className='start_shopping_button'>Start Shopping</button>
-            </div>
-            <div className="items_container">
-                {items.map((item) => {
-                    return ( <GroceryListItem item={item}/>
-                    )
-                })}
-            </div>
-            </Container>
+                <Container style={{ backgroundColor: "#FFF8F0" }}>
+                    <div>
+                        <input className=' input-reset ba bg-transparent w-100 measure add_product_input' type="text" id="products" value={value}
+                            placeholder='Search for products'
+                            onChange={(event) => setValue(event.target.value)}></input>
+                    </div>
+
+                    <div>
+                        <button className='add_product_button' onClick={onAddProduct} type='submit'>Add Product</button>
+                    </div>
+                </Container>
+
+                <CardActions style={{ backgroundColor: "#FFF8F0" }}>
+
+                    <Button style={{ color: "#FFF8F0" }} size="small" onClick={saveList} >Save List</Button>
+                    <Button style={{ color: "#FFF8F0" }} size="small">Start Shopping</Button>
+                    <div className="items_container">
+                        {items.map((item) => {
+                            return (<GroceryListItem item={item} />
+                            )
+                        })}
+                    </div>
+                </CardActions>
             </React.Fragment>
 
 
--- grocerease/src/components/Homepage.js
+++ grocerease/src/components/Homepage.js
@@ -1,20 +1,27 @@
 import { Link } from "react-router-dom";
 import '../homepage.css'
+import { Typography, Container } from "@mui/material";
+
 
 const Homepage = () => {
 
-return (
+    return (
         <div>
             <div>
-                <header>Welcome to Grocerease</header>
+            <Container style={{backgroundColor: "#EEB61B"}}>
+                <Typography variant="h3" color="#FFF8F0" gutterBottom component="div">
+                Welcome to Grocerease
+                </Typography>
+            </Container>
             </div>
-                <div className="login_button">
-                    <Link to='/login'>Login</Link>
-                </div>
-                <div className="register_button">
-                    <Link to='/register'>Register</Link>
-                </div>
+            <div className="login_button">
+                <Link to='/login'>Login</Link>
+            </div>
+            <div className="register_button">
+                <Link to='/register'>Register</Link>
+            </div>
         </div>
-)}
+    )
+}
 
 export default Homepage;
