257edf09a2fd9676c52ab3a07cc30535fea00d3c
Jordyn <jordyn@example.com>
2022-01-30 11:19:48 -0500

    commeted out the div to resolve netlify issues

--- grocerease/src/components/CreateListForm.js
+++ grocerease/src/components/CreateListForm.js
@@ -38,7 +38,7 @@ const CreateListForm = ({ token }) => {
     };
 
     return (
-        <div>
+        // <div>
             <React.Fragment>
                 <CssBaseline />
 
@@ -63,7 +63,7 @@ const CreateListForm = ({ token }) => {
                     </div>
                 </Container>
             </React.Fragment>
-        </div>
+        // </div>
     );
 };
 
